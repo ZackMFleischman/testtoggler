8422b6e0921a3c036a80ac94469f41720ba0e920
Zack M Fleischman <zFleischman@codecademy.com>
2021-05-03 17:04:21 -0700

    Works with other extensions

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,32 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 
+const findFile = (fileGlob: string) => vscode.workspace.findFiles(fileGlob, "/node_modules/", 10)
+    .then(uris => {
+      uris.map(uri => console.log(uri.fsPath));
+      return uris.length > 0 ? uris[0] : null;
+    });
+
+// If test && is javascript/typescript:
+//    Extract baseFilename from test file.
+//    If the grandparent folder name === baseFilename, then look for `../index.{ext}`
+//    else look for `../{baseFilename}.{ext}`
+//    if nothing, then look globally for {baseFilename}.{ext}, 
+//
+//     choices = [index.tsx, ../filename.tsx, globalSearch]
+//
+//     for each choice:
+//        if (1) choice, then go to it.
+      //  if multiple choices, then present them in a picker to the user,
+      //  if no choices, try the next one in the list, and pop info message to user if there's none left.
+
+
+// If source && is javascript/typescript:
+//    Extract baseFilename from source file (if it's 'index', use the parent folder for this).
+//    Search for baseFilename.{ts(x), js(x)} in the following
+
+
+// NEXT: Get it to work for multiple file extensions.
 
 
 async function openFileFrom(filePath: string) {
@@ -25,12 +51,12 @@ export function activate(context: vscode.ExtensionContext) {
     const toUri = (relativeWorkspacePath: string) => vscode.Uri.file(path.join(workspacePath, relativeWorkspacePath));
     const fileExists = async (filePath: string) => {
       try {
-        await vscode.workspace.fs.stat(toUri(filePath))
+        await vscode.workspace.fs.stat(toUri(filePath));
         return true;
       } catch (err) {
         return false;
       }
-    }
+    };
 
     const baseName = path.basename(currentFilename, path.extname(currentFilename));
     const baseFolder = path.relative(workspacePath, path.dirname(document.fileName));
@@ -38,44 +64,51 @@ export function activate(context: vscode.ExtensionContext) {
 
     console.log('BASE FOLDER: ' + baseFolder);
 
-    let openFileName = '';
-    const isTestFile = /.*-test\.ts/.test(path.basename(currentFilename));
-    if (isTestFile)
+    const isTestFileRegex = /(.+)[-_\.](tests?|specs?)\..+/i;
+
+    let fileGlob = '';
+    const match = path.basename(currentFilename).match(isTestFileRegex);
+    const isTestFile = match !== null;
+    if (match !== null)
       {
-        console.log('Is a test file!');
-        const baseNameMinusTest = baseName.slice(0, baseName.length-5);
+        const baseNameMinusTest = match[1];
+        console.log(`Is a test file! baseName = ${baseNameMinusTest}`);
         const folderName =  path.basename(path.dirname(baseFolder));
 
         if (folderName !== baseNameMinusTest)
-          openFileName =  path.join(path.dirname(baseFolder), `${baseNameMinusTest}.tsx`);
+          fileGlob =  path.join(path.dirname(baseFolder), `${baseNameMinusTest}.*`);
         else 
-          openFileName =  path.join(path.dirname(baseFolder), `index.tsx`);
+          fileGlob =  path.join(path.dirname(baseFolder), `index.*`);
       } else {
         console.log('Is a source file!');
         if (baseName === 'index') {
           console.log('Current file is an index file!');
-          console.log(`Looking for ./__tests__/${parentFolderName}-test.tsx`);
-          openFileName = path.join(baseFolder, '__tests__', `${parentFolderName}-test.tsx`);
+          console.log(`Looking for ./__tests__/${parentFolderName}-test.*`);
+          fileGlob = path.join(baseFolder, '__tests__', `${parentFolderName}-test.*`);
         } else {
-          openFileName = path.join(baseFolder, '__tests__', `${baseName}-test.tsx`);
+          fileGlob = path.join(baseFolder, '__tests__', `${baseName}-test.*`);
         }
       }
 
-
-
+    
+    console.log(`openFileName: ${fileGlob}`);
+    
+    const fileUri = await findFile(fileGlob);
+    // const fileUri = await findFile('**/' + openFileName);
+    // const include = `**/__tests__/${parentFolderName}-test.tsx`;
     // const config = vscode.workspace.getConfiguration('testToggler');
+    // console.log(`Full folder name: ${path.dirname(currentFilename)}`);
 
-
-    console.log(`Full folder name: ${path.dirname(currentFilename)}`);
     console.log("----------------");
-    console.log(`Trying to open file: ${openFileName}`);
+    console.log(`Trying to open file: ${fileUri?.fsPath}`);
 
-    if (await fileExists(openFileName))  {
-      console.log(`File exists: ${openFileName}`);
-      await openFileFrom(path.join(workspacePath, openFileName));
+    if (fileUri !== null)  {
+      console.log(`File exists: ${fileUri.fsPath}`);
+      await openFileFrom(fileUri.fsPath); // path.join(workspacePath, openFileName));
     }
     else  {
       console.log('File does not exist!');
+      vscode.window.showErrorMessage(`Could not find ${isTestFile ? 'source' : 'test'} file for ${path.relative(workspacePath, document.fileName)}`);
       return;
     }
     console.log("----------------");
@@ -83,14 +116,14 @@ export function activate(context: vscode.ExtensionContext) {
 
 
     // const include = `**/__tests__/${parentFolderName}-test.tsx`;
-    const exclude = "/node_modules/";
+    // const exclude = "/node_modules/";
 
-    vscode.workspace.findFiles(openFileName, exclude, 10)
-      .then((uris: vscode.Uri[]) => {
-        uris.map(uri => uri.fsPath).forEach(
-          file => console.log(file)
-        );
-      }); 
+    // vscode.workspace.findFiles(openFileName, exclude, 10)
+    //   .then((uris: vscode.Uri[]) => {
+    //     uris.map(uri => uri.fsPath).forEach(
+    //       file => console.log(file)
+    //     );
+    //   }); 
 
 		// vscode.window.showInformationMessage('Hello World from TestToggler!');
 	});
